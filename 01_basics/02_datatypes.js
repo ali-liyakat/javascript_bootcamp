92fde62778a3a697420cf7e20907eb85b4671b22
Liyakat ali <105539989+ali-liyakat@users.noreply.github.com>
2022-12-27 15:35:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";       //treat all JS code as newer version,,but it comes default now
+
+//alert("hello")    // not used in node
+
+//Datatypes
+let myName ="Ali"     //string
+let age=22          //number
+let isMan=true      //boolean
+// null => standalone value ,empty    ,, it is of type Object
+// undefined => value not defined for variable,, it is of type Undefined
+// symbol => uniqueness
+// object
+console.log(typeof(myName))     // string
+console.log(typeof(age))        // number
+console.log(typeof(isMan))     // boolean
+console.log(typeof(null))     // object
+console.log(typeof(undefined))   // undefined
+console.log(typeof(Symbol))     //function
