46533e73436d285615fdbf5e2fe54b20faa2e0bd
Liyakat ali <105539989+ali-liyakat@users.noreply.github.com>
2023-06-06 08:00:58 +0000

    details about strings anf their methods

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,14 @@
+const myname="ali"      // we can use single or double quotes.
+const age=22;           // ** -> string is an object
+
+console.log(`My name is ${myname} and my age is ${age}`);   //modern way
+
+const userName= new String("Liyakat");      // string object
+// we can access the string values using index notation []
+
+// ******** String Methods ***************************
+// use dot notation to acces the string methods.
+
+console.log(userName.length);
+//  .charAt(), .indexOf(), .substring(), .slice(), .trim(), .replace(), .includes(), .split()
+// MDN docs for various string methods.
