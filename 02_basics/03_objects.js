6459b4e97cf74af5cfe23c30c8df6b3905827f7e
Liyakat ali <105539989+ali-liyakat@users.noreply.github.com>
2023-06-08 11:07:08 +0000

    object destructuring

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,21 @@
+// object destructuring
+const course = {
+    name: "js",
+    price: "99",
+    tutor: "Hitesh"
+}
+const {tutor:teacher} = course;
+//console.log(tutor);
+console.log(teacher);
+
+
+// Json Format
+// {
+//     "name": "ali",
+//     "age": "22"
+// }
+
+// [                this can also we get.
+//     {},
+//     {}
+// ]
