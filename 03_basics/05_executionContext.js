ddad8cf342c5210cee48ff8d1d3a9e471afd1e4a
Liyakat ali <105539989+ali-liyakat@users.noreply.github.com>
2023-06-09 10:22:31 +0000

    Execution Context and Call Stack

---
+++ 03_basics/05_executionContext.js
@@ -0,0 +1,14 @@
+// Javascript execution context
+
+//  -> Global Execution Context  (this)
+//      -> all variables are set undefined
+//  -> Function Execution Contect
+//  -> Eval execution context
+//      -> code excution
+
+// JS code runs in 2 phases
+//      -> Memory creation phase
+//      -> Execution phase
+
+//      Call Stack
+//      all functions go in the call stack one by one and run, after complete execution, they are deleted from call stack.
