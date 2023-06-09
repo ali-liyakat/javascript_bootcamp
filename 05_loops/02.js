c57382bdee7d39852c43bd14a063fcef3a1f99e2
Liyakat ali <105539989+ali-liyakat@users.noreply.github.com>
2023-06-09 17:18:29 +0000

    while and do-while loop in JS

---
+++ 05_loops/02.js
@@ -0,0 +1,21 @@
+//  while and do-while loop
+
+let index = 0;
+while(index <= 5){
+    console.log(index)
+    index++
+}
+
+let arr = [10,20,30,40]
+let i = 0;
+while(i < arr.length){
+    console.log(arr[i])
+    i++
+}
+
+
+let score = 0;
+do {
+    console.log(score)  // runs atleat once
+    score++
+} while (score <= 5);
